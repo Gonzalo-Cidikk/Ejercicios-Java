857b8f3de49861cb60e2ea9f0bd959a7bef6a39b
“Gonzalo-Cidikk” <gonzalogribaudo01@gmail.com>
2022-09-01 09:37:47 -0300

    Hola Mundo

---
+++ Hola_Mundo/src/hola_mundo/Hola_Mundo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola_mundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Hola_Mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Se crea una variable Scanner que se utiliza para leer datos del usuario
+        Scanner leer = new Scanner(System.in); 
+        //Se crea una variable de tipo cadena que se utiliza para leer datos del usuario
+        String nombre, apellido;
+        //Se pide que se ingrese el Nombre del usuario
+        System.out.println("Ingresa tu nombre");
+        //Leemos el nombre de la persona en la variable nombre
+        nombre = leer.next();
+        //
+        System.out.println("Ingrese tu apellido");
+        //
+        apellido = leer.next();
+        //Mostramos por pantalla el mensaje de Hola mundo! con el nombre de la persona
+        System.out.println("Hola Mundo! soy " + nombre + " " + apellido + " y estoy programando en java!");
+    }
+    
+}
