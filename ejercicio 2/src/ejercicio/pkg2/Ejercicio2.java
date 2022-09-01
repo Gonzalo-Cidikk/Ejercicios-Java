da5a06951aabe9027daa6819d99a75bec73ddfac
“Gonzalo-Cidikk” <gonzalogribaudo01@gmail.com>
2022-09-01 11:04:28 -0300

    Primera tanda de Ejercicios

---
+++ ejercicio 2/src/ejercicio/pkg2/Ejercicio2.java
@@ -0,0 +1,17 @@
+
+package ejercicio.pkg2;
+
+import java.util.Scanner;
+
+
+public class Ejercicio2 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Ingrese su nombre");
+        Scanner leer = new Scanner(System.in);
+        String nombre = leer.nextLine();
+        System.out.println("Hola "+nombre+" te damos la bienvenida!");
+    }
+    
+}
